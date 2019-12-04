9c8188a5ede08a3f1382e9b2b69f26cc893b8465
mher.paruyryan <mher.paruyryan@zangi.com>
2019-12-04 23:42:31 +0400

    added task delete and update method

--- src/main/java/dev/mher/taskhunter/controllers/v1/TaskController.java
+++ src/main/java/dev/mher/taskhunter/controllers/v1/TaskController.java
@@ -1,6 +1,5 @@
 package dev.mher.taskhunter.controllers.v1;
 
-import dev.mher.taskhunter.models.ProjectModel;
 import dev.mher.taskhunter.models.TaskModel;
 import dev.mher.taskhunter.models.misc.task.Task;
 import dev.mher.taskhunter.services.TaskService;
@@ -9,10 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * User: MheR
@@ -75,7 +75,7 @@ public class TaskController {
 
 
     @GetMapping("/{taskId}")
-    public ResponseEntity retrieveProject(
+    public ResponseEntity retrieveTask(
             @PathVariable("taskId") Integer taskId
     ) {
         try {
@@ -84,6 +84,47 @@ public class TaskController {
                 return ResponseEntity.ok(new ResponseUtils(true, "RETRIEVE_ERROR"));
             }
             return ResponseEntity.ok(new ResponseUtils(task));
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+            logger.error(e.getMessage(), e);
+        }
+        return ResponseEntity.ok(new ResponseUtils(true, "UNKNOWN_ERROR"));
+    }
+
+
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity updateTask(
+            @PathVariable("taskId") Integer taskId,
+            @RequestBody Task task
+    ) {
+        try {
+            task.setTaskId(taskId);
+            TaskModel taskModel = taskService.update(task);
+            if (taskModel == null) {
+                return ResponseEntity.ok(new ResponseUtils(true, "UPDATE_ERROR"));
+            }
+            return ResponseEntity.ok(new ResponseUtils(taskModel));
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+            logger.error(e.getMessage(), e);
+        }
+        return ResponseEntity.ok(new ResponseUtils(true, "UNKNOWN_ERROR"));
+    }
+
+
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity deleteTask(
+            @PathVariable("taskId") Integer taskId
+    ) {
+        try {
+            Boolean isDeleted = taskService.delete(taskId);
+            if (isDeleted == null) {
+                return ResponseEntity.ok(new ResponseUtils(true, "DELETE_ERROR"));
+            }
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("isDeleted", isDeleted);
+            return ResponseEntity.ok(new ResponseUtils(response));
         } catch (Exception e) {
             logger.info(e.getMessage());
             logger.error(e.getMessage(), e);
--- src/main/java/dev/mher/taskhunter/models/TaskModel.java
+++ src/main/java/dev/mher/taskhunter/models/TaskModel.java
@@ -174,4 +174,111 @@ public class TaskModel extends Task {
         }
         return null;
     }
+
+    public TaskModel update() {
+        String queryString = "UPDATE tasks\n" +
+                "SET project_id=?, parent_task_id=?, name=?, text=?, updated_at=?\n" +
+                "WHERE task_id=?;";
+        Connection con = null;
+        PreparedStatement pst = null;
+        boolean isUpdated = false;
+        try {
+            con = this.getDataSource().getConnection();
+            pst = con.prepareStatement(queryString);
+
+            pst.setInt(1, this.getProjectId());
+            pst.setInt(2, this.getParentTaskId());
+            pst.setString(3, this.getName());
+            pst.setString(4, this.getText());
+            pst.setTimestamp(5, this.getUpdatedAt());
+            pst.setInt(6, this.getTaskId());
+
+            isUpdated = pst.executeUpdate() != 0;
+            if (isUpdated) {
+                return this;
+            }
+        } catch (SQLException e) {
+            logger.info(e.getMessage());
+            logger.error(e.getMessage(), e);
+        } finally {
+            DataSourceUtils.closeConnection(con, pst, null);
+        }
+        return null;
+    }
+
+
+    public boolean delete() throws SQLException {
+        Connection conn = null;
+        try {
+            conn = this.getDataSource().getConnection();
+            conn.setAutoCommit(false);
+            // delete owners
+            this.deleteSubTasks(conn);
+            this.deleteTaskById(conn);
+            // finalize transaction
+            conn.commit();
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            logger.info(e.getMessage(), e);
+            if (conn != null) {
+                conn.rollback();
+            }
+        } finally {
+            DataSourceUtils.closeConnection(conn, null, null);
+        }
+        return true;
+    }
+
+    private boolean deleteSubTasks(Connection conn) throws SQLException {
+        boolean isInTransaction = conn != null;
+        if (!isInTransaction) {
+            conn = this.getDataSource().getConnection();
+        }
+        String queryString = "DELETE FROM tasks WHERE parent_task_id=?;";
+        PreparedStatement pst = null;
+        boolean isDeleted = false;
+        try {
+            pst = conn.prepareStatement(queryString);
+            pst.setInt(1, this.getTaskId());
+            isDeleted = pst.execute();
+            if (isInTransaction) {
+                DataSourceUtils.closeConnection(null, pst, null);
+            }
+        } catch (SQLException e) {
+            logger.info(e.getMessage());
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (!isInTransaction) {
+                DataSourceUtils.closeConnection(conn, pst, null);
+            }
+        }
+        return isDeleted;
+    }
+
+    private boolean deleteTaskById(Connection conn) throws SQLException {
+        boolean isInTransaction = conn != null;
+        if (!isInTransaction) {
+            conn = this.getDataSource().getConnection();
+        }
+        String queryString = "DELETE FROM tasks WHERE task_id=?;";
+        PreparedStatement pst = null;
+        boolean isDeleted = false;
+        try {
+            pst = conn.prepareStatement(queryString);
+            pst.setInt(1, this.getTaskId());
+            isDeleted = pst.execute();
+            if (isInTransaction) {
+                DataSourceUtils.closeConnection(null, pst, null);
+            }
+        } catch (SQLException e) {
+            logger.info(e.getMessage());
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (!isInTransaction) {
+                DataSourceUtils.closeConnection(conn, pst, null);
+            }
+        }
+        return isDeleted;
+    }
+
 }

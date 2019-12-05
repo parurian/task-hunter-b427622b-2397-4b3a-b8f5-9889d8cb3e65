7f582430a9d5cf3eecf897a8051aecf98243e14d
mher.paruyryan <mher.paruyryan@zangi.com>
2019-12-05 21:50:26 +0400

    properties to yml and changed banner

--- src/main/java/dev/mher/taskhunter/config/SecurityConfig.java
+++ src/main/java/dev/mher/taskhunter/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .cors()
                 .and()
-                    .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(authenticationFilter, BasicAuthenticationFilter.class)
                     .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
                     .sessionManagement()

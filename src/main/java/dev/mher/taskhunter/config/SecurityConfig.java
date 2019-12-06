b8baf8cccd329f4e7f6cdf27cacf58c27d361e2e
mher.paruyryan <mher.paruyryan@zangi.com>
2019-12-07 00:24:48 +0400

    changed auth filter from bean to non bean

--- src/main/java/dev/mher/taskhunter/config/SecurityConfig.java
+++ src/main/java/dev/mher/taskhunter/config/SecurityConfig.java
@@ -1,23 +1,33 @@
 package dev.mher.taskhunter.config;
 
 import dev.mher.taskhunter.controllers.filters.AccessDeniedHandler;
-import dev.mher.taskhunter.controllers.filters.AuthenticationFilter;
+import dev.mher.taskhunter.services.JwtService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 
 /**
  * User: MheR
@@ -39,10 +49,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${cors.allowed.headers}")
     private String corsAllowedHeaders;
 
-    private final AuthenticationFilter authenticationFilter;
+    private final JwtService jwtService;
 
-    public SecurityConfig(AuthenticationFilter authenticationFilter) {
-        this.authenticationFilter = authenticationFilter;
+    public SecurityConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -60,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers(HttpMethod.POST, "/v1/authentication/**").permitAll()
                 .anyRequest().authenticated();
-        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(accessFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
@@ -91,4 +101,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new AccessDeniedHandler();
     }
 
+    @Bean
+    public OncePerRequestFilter accessFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                Optional.ofNullable(request.getHeader("X-Access-Token"))
+                        .flatMap(token -> Optional.ofNullable(jwtService.decode(token)))
+                        .ifPresent(output -> {
+                            Integer userId = Integer.parseInt(output);
+                            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                        userId,
+                                        null,
+                                        Collections.emptyList()
+                                );
+                                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                            }
+                        });
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+
 }
+
+
